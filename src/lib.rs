c4a6ad0e939eaf1a798c060639d1daa566f86427
Alvaro Berenguer <alvaroberenguer@gmail.com>
2025-09-08 21:27:27 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
 #[macro_export]
+/// Executes multiple closures in parallel using `rayon::join`.
+/// ## Usage
+/// You can pass any number of closures:
+/// ```
+/// rayon_join::join!(|| { /* task 1 */ }, || { /* task 2 */ }, || { /* task 3 */ });
+/// ```
+/// You can also pass functions:
+/// ```
+/// fn task1() { /* ... */ }
+/// fn task2() { /* ... */ }
+/// fn task3() { /* ... */ }
+/// rayon_join::join!(task1, task2, task3);
+/// ```
+/// If you need to pass arguments to the functions, use a closure:
+/// ```
+/// fn task_with_arg(x: i32) { /* ... */ }
+/// rayon_join::join!(|| task_with_arg(42), || task_with_arg(7));
+/// ```
 macro_rules! join {
     ($f1:expr, $f2:expr) => {
         rayon::join($f1, $f2)
